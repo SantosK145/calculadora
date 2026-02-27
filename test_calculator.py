aef0788fcb8dc26fe90aca43bede2029404e9753
SantosK145 <Jesus2020.jsasa@gmail.com>
2026-02-27 10:31:56 -0700

    Creacion de test_calculator

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import sumar, restar, multiplicar, dividir
+
+def test_sumar():
+    assert sumar(2, 3) == 5
+
+def test_restar():
+    assert restar(5, 2) == 3
+
+def test_multiplicar():
+    assert multiplicar(3, 4) == 12
+
+def test_dividir():
+    assert dividir(10, 2) == 5
